2386db4b37c3f171b7a004e98029c6ef8a623bfd
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-07 11:46:44 +0000

    Updates

---
+++ precache-manifest.65b878d3bba2327142293d8bebfa9849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e003aaba36c3bc441927f5bee746c442",
+    "url": "/reactquiz/index.html"
+  },
+  {
+    "revision": "bb96c6e23ff8727b9242",
+    "url": "/reactquiz/static/css/main.cb2bfce2.chunk.css"
+  },
+  {
+    "revision": "1591b9176972ac97ddd1",
+    "url": "/reactquiz/static/js/2.e2595d9e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactquiz/static/js/2.e2595d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb96c6e23ff8727b9242",
+    "url": "/reactquiz/static/js/main.013aa0dc.chunk.js"
+  },
+  {
+    "revision": "ff36473764f04744d47e",
+    "url": "/reactquiz/static/js/runtime-main.b38bf0a8.js"
+  }
+]);
