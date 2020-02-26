8278801855b89b9fe9523fb8920e719a3d7213d6
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-26 17:22:51 +0000

    finalizing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,74 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Result from "./components/Result"
+import quizService from './quizService';
+import "./assets/style.css";
+import QuestionBox from './components/QuestionBox';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Quiz extends Component {
+    state = {
+        questionBank:[],
+        score: 0,
+        responses: 0
+    };
+    getQuestions = () =>{
+        quizService().then(question=>{
+            this.setState({
+                questionBank: question
+            });
+        });
+    };
+    computeAnswer = (answer, correctAnswer)=>{
+        if(answer === correctAnswer){
+            this.setState({
+                score: this.state.score + 1
+            })
+        }
+        this.setState({
+            responses: this.state.responses < 10 ? this.state.responses +1 : 10
+        })
+    }
+    playAgain = ()=>{
+        this.getQuestions();
+        this.setState({
+            score:0,
+            responses:0
+        })
+    }
+    getQuestions = () =>{
+        quizService().then(question =>{
+            this.setState({
+                questionBank: question
+            })
+        })
+    }
+    componentDidMount(){
+        this.getQuestions();
+        
+    }
+    render(){
+        return(
+            <div className="container">
+                <div className="title">Premier league quiz!</div>
+                {this.state.questionBank.length > 0 && 
+                this.state.responses < 10 &&
+                this.state.questionBank.map(
+                    ({question, answers, correct, questionId})=> (
+                        <QuestionBox
+                        question={question}
+                        options={answers}
+                        key={questionId}
+                        selected={answer => this.computeAnswer(answer, correct)}
+                        />
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+                    )
+                )}
+
+                {this.state.responses === 10? (
+                    <Result score = {this.state.score} playAgain = {this.playAgain} />
+                    ):null}
+            </div>
+        );
+    }
+}
+ReactDOM.render(<Quiz />, document.getElementById("root"));
---
+++ src/quizService/index.js
@@ -0,0 +1,328 @@
+const qBank = [
+    {
+      question:
+        "Which premier league team has the record of 13 FA cup titles?",
+      answers: ["Chelsea", "Liverpool", "Manchester united", "Arsenal"],
+      correct: "Arsenal",
+      questionId: "099099"
+    },
+    {
+      question:
+        'Who is the premier leagues all time goal scorer?',
+      answers: ["Alan Shearer", "Frank Lampard", "Wayne Rooney", "Cristiano Ronaldo"],
+      correct: "Alan Shearer",
+      questionId: "183452"
+    },
+    {
+      question:
+        "Which of the following players have played both for Manchester City and Manchester United?",
+      answers: ["David Silva", "Steven Gerrard", "Carlos Tevez", "Owen hargreeves"],
+      correct: "Carlos Tevez",
+      questionId: "267908"
+    },
+    {
+      question: 
+      "Which one of the following players was not in the premier league team of the year in the season 2019/ 2020?",
+      answers: [
+        "Sergio Aguero",
+        "Paul Pogba",
+        "Harry Kane",
+        "Sadio Mane"
+      ],
+      correct: "Harry Kane",
+      questionId: "333247"
+    },
+    {
+      question: 
+      "Which player kicked Manchester city's goal keeper Ederson in the face?",
+      answers: ["Mo Salah", "Joey Barton", "Sadio Mane", "Marcus Rashford"],
+      correct: "Sadio Mane",
+      questionId: "496293"
+    },
+    {
+      question:
+        "Which premier league side has the most premier league titles with 13 times?",
+      answers: [
+        "Manchester United",
+        "Chelsea",
+        "Liverpool",
+        "Arsenal"
+      ],
+      correct: "Manchester United",
+      questionId: "588909"
+    },
+    {
+      question:
+        'Which stadium has the least seats out of the following premier league grounds?',
+      answers: ["Vitality Stadium", "Turf Moor", "Vicarage Road", "Selhurst Park"],
+      correct: "Vitality Stadium",
+      questionId: "648452"
+    },
+    {
+      question: 
+      "Which premier league stadium has the most seating capacity?",
+      answers: ["Old Trafford", "The Etihad", "Emirates", "The tottenham hotspur stadium"],
+      correct: "Old Trafford",
+      questionId: "786649"
+    },
+    {
+      question:
+        "Who did man utd sign on the 31st January 2020 ?",
+      answers: ["Odion Ighalo", "Slimani", "Bruno Fernandez", "Leo Messi"],
+      correct: "Odion Ighalo",
+      questionId: "839754"
+    },
+    {
+      question:
+        "Which premier league player got the goal of the season in 2018/19 season?",
+      answers: [
+        "Troy Deeny",
+        "Heung Min Son",
+        "Andros Townsend",
+        "Mo Salah"
+      ],
+      correct: "Andros Townsend",
+      questionId: "98390"
+    },
+    {
+      question: 
+      "Who is Norwich manager?",
+      answers: ["Eddie Howe", "Daniel Farke", "Emi Buendia", "Roy Hodgeson"],
+      correct: "Daniel Farke",
+      questionId: "1071006"
+    },
+    {
+      question:
+       "Who was bournemouths top premier league goalscorer in 2018/19?",
+      answers: ["Joshua King", "Nathan Ake", "Callum Wilson", "Steve Cook"],
+      correct: "Callum Wilson",
+      questionId: "1174154"
+    },
+    {
+      question:
+       "3 players got the golden boot in the season of 2018/19 season, which of the following did not get the golden boot?",
+      answers: ["Mo Salah", "Sadio Mane", "Aubameyang", "Sergio Aguero"],
+      correct: "Sergio Aguero",
+      questionId: "1226535"
+    },
+    {
+      question:
+       'Which premier league side where known as "The invincibles" in the 2003/04 season?',
+      answers: ["Chelsea", "Manchester United", "Arsenal", "Liverpool"],
+      correct: "Arsenal",
+      questionId: "1310938"
+    },
+    {
+      question:
+       "Which team came 20th in the premier league 2017/18 season?",
+      answers: ["Westbromwich albion", "Stoke city", "Southampton", "Cardif city"],
+      correct: "Westbromwich albion",
+      questionId: "1436873"
+    },
+    {
+      question:
+       "Which premier league side where known as the centurians when they hit a record of 100 points in the premier league?",
+      answers: ["Manchester City", "Manchester United", "Arsenal", "Liverpool"],
+      correct: "Manchester City",
+      questionId: "1515110"
+    },
+    {
+      question:
+       "Who won the premier league player of the year in 2019?",
+      answers: ["Mo salah", "Sergio Aguero", "Virgil Van Dijk", "Anthony Martial"],
+      correct: "Virgil Van Dijk",
+      questionId: "1642728"
+    },
+    {
+      question:
+        "Who is Chelsea's record signing? ",
+      answers: ["Eden Hazard", "Juan Mata", "Didier Drogba", "Kepa Arrizabalaga"],
+      correct: "Kepa Arrizabalaga",
+      questionId: "1747256"
+    },
+    {
+      question:
+        "Who is Manchester United's record signing?",
+      answers: ["Paul Pogba", "Harry Maguire", "Cristiano Ronaldo", "David Beckham"],
+      correct: "Paul Pogba",
+      questionId: "1822532"
+    },
+    {
+      question: 
+      "Who is the newcastle manager?",
+      answers: [
+        "Sean Dyce",
+        "Steve Bruce",
+        "Tammy Abraham",
+        "Nigel Pearson"
+      ],
+      correct: "Steve Bruce",
+      questionId: "195075"
+    },
+    {
+      question: 
+      "Who scored a hatrick on  City's first game of the 2019/20 season against Westham?",
+      answers: ["Sergio Aguero", "Raheem Sterling", "Gabriel Jesus", "Kevin De Bruyne"],
+      correct: "Raheem Sterling",
+      questionId: "2019778"
+    },
+    {
+      question: 
+      "In what season did leads last play in the premier league?",
+      answers: ["2016/17", "2005/06", "2003/04", "2007/08"],
+      correct: "2003/04",
+      questionId: "2134343"
+    },
+    {
+      question: 
+      "Who has the most premier league assists of all time?",
+      answers: [
+        "Ryan Giggs",
+        "Kevin De Bruyne",
+        "Wayne Rooney",
+        "Jack Grealish"
+      ],
+      correct: "Ryan Giggs",
+      questionId: "2210799"
+    },
+    {
+      question:
+        "Who has made the most premier league tackles of all time?",
+      answers: ["Virgil van dijk", "Wilfred Ndidi", "Gareth Barry", "Mark noble"],
+      correct: "Gareth Barry",
+      questionId: "2379831"
+    },
+    {
+      question: 
+      "Who has the most saves in the premier league of all time?",
+      answers: ["Edwin Van Der Sar", "Ben Foster", "Petr Cech", "David De Gea"],
+      correct: "Ben Foster",
+      questionId: "2426418"
+    },
+    {
+      question: 
+      "Which player has been unlucky enough to strike the woodwork 44 times?",
+      answers: ["Robin Van Persie", "Wayne Rooney", "Phil Jones", "Steven Gerrard"],
+      correct: "Robin Van Persie",
+      questionId: "2510086"
+    },
+    {
+      question: 
+      "Who has won the most premier league player of the month awards at 7?",
+      answers: [
+        "Heung Min Son",
+        "Sergio Aguero",
+        "Mo salah",
+        "Paul pogba"
+      ],
+      correct: "Sergio Aguero",
+      questionId: "2685745"
+    },
+    {
+      question:
+        "Which premier league player has picked up a record of 123 yellow cards in his career?",
+      answers: ["Paul Scholes", "Frank Lampard", "Gareth Barry", "Mustafi"],
+      correct: "Gareth Barry",
+      questionId: "2796884"
+    },
+    {
+      question: 
+      "Who won the premier league in 2015/16 season?",
+      answers: ["Leicester city", "Manchester City", "Chelsea", "Arsenal"],
+      correct: "Leicester city",
+      questionId: "2838900"
+    },
+    {
+      question: "Which premier league team scored the most goals in the 2018/19 season?",
+      answers: ["Manchester city", "Manchester United", " Liverpool", "Wolves"],
+      correct: "Manchester city",
+      questionId: "298060"
+    },
+    {
+      question: "Who is the heighest paid player in the premier league?",
+      answers: ["Alexis Sanchez", "Paul Pogba", "Sergio Aguero", "Virgil Van Dijk"],
+      correct: "Alexis Sanchez",
+      questionId: "3096579"
+    },
+    {
+      question: "Huddersfield town had an awful 2018/19 season, how many points did they get after 38 games?",
+      answers: [
+        "11",
+        "14",
+        "16",
+        "22"
+      ],
+      correct: "16",
+      questionId: "3182461"
+    },
+    {
+      question: "Who were the 1999 treble winners?",
+      answers: [
+        "Leeds Unitd",
+        "Manchester United",
+        "Arsenal",
+        "Liverpool"
+      ],
+      correct: "Manchester United",
+      questionId: "3239112"
+    },
+    {
+      question: "Which of the following teams have never been relegated from the premier league?",
+      answers: ["Leicester city", "Everton", "Manchester city", "Burnley"],
+      correct: "Everton",
+      questionId: "3318503"
+    },
+    {
+      question:
+        "Which keeper saved 2 penalties against Manchester united in the season 2019/20?",
+      answers: ["Nic Pope", "Tim Krul", "Alisson Becker", "Kepa"],
+      correct: "Tim Krul",
+      questionId: "3410327"
+    },
+    {
+      question: "What national team does Pierre-Emerick Aubameyang play for",
+      answers: ["France", "Nigeria", "Gabon", "Chad"],
+      correct: "Gabon",
+      questionId: "3542596"
+    },
+    {
+      question: "Who has scored the most own goals in premier league history?",
+      answers: ["Phil Jones", "Dennis Erwin", "Rio Ferdinand", "Richard Dunne"],
+      correct: "Richard Dunne",
+      questionId: "3667517"
+    },
+    {
+      question: "Who has had the most offsides in premier league history?",
+      answers: [
+        "Berbatov",
+        "Adebayor",
+        "Benteke",
+        "Aguero"
+      ],
+      correct: "Adebayor",
+      questionId: "3791672"
+    },
+    {
+      question: "In the season of 2018/19 how many players played every minute of every game?",
+      answers: ["10", "0", "5", "12"],
+      correct: "12",
+      questionId: "3893585"
+    },
+    {
+      question: "Who got the most assists in 2018/19 season?",
+      answers: ["Kevin De Bruyne", "Eden Hazard", "Juan Mata", "Willian"],
+      correct: "Eden Hazard",
+      questionId: "3913430"
+    },
+    {
+      question: "Which keeper had the most clean sheets in the 2018/19 season?",
+      answers: ["Alisson", "Ederson", "Kepa", "De Gea"],
+      correct: "Alisson",
+      questionId: "4049121"
+    },
+    
+  ];
+  
+  export default (n = 10) =>
+    Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
+  
